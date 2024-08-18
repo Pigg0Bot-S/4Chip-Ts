da33866ede32f9714e0267fff758815921fa3c8e
Pigg0Bot <>
2024-08-18 16:01:15 -0400

    Begin work on board

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,15 @@ import './style.css'
 import typescriptLogo from './typescript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.ts'
+import 
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
     <h1>Welcome to 4Chip!<h1>
+	
   </div>
+  <div id="game"></div>
 `
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+
+const game_window : HTMLDivElement | null = document.querySelector<HTMLDivElement>('#game')
